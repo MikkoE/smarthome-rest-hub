17814603803d979cd22dbe8ff194ae76d33d5565
mieb <mikko.eberhardt@ibeo-as.com>
2022-06-25 22:00:52 +0200

    adding on off routines for kitchen

--- node_server/app.js
+++ node_server/app.js
@@ -8,5 +8,6 @@ app.use(express.json())
 
 //Routes
 app.use('/i3', require('./routes/index'));
+console.log("running on: IP:8080");
 
 app.listen(8080);
--- node_server/routes/index.js
+++ node_server/routes/index.js
@@ -1,26 +1,47 @@
 //index file for the i3 routesa
 const express = require('express');
 const router  = express.Router();
+const https = require('https')
 
-let request = require('request')
+const axios = require('axios');
+
 
 router.get('/:scene', (req,res)=>{
     console.log('request was made: ' + req.url)
-    res.writeHead(200, {'Content-Type': 'application/json'});
+    //res.writeHead(200, {'Content-Type': 'application/json'});
     
-    var screne = req.params.scene;
+    var scene = parseInt(req.params.scene);
+    console.log(typeof(scene));
     switch (scene) {
-        case 1: // wohnzimmer decke an
+        case 1: // küche an
+            console.log("turning kitchen lights on")
             var body = {"on": true}
-            // url values /api/-kft5OkDjuXXzojboZxroWDrwkyNqP57cSm6PJhl/groups/1/action
-            request.put()
-            
+
+            axios.put('http://192.168.178.136/api/-kft5OkDjuXXzojboZxroWDrwkyNqP57cSm6PJhl/groups/1/action', body)
+                .then((res) => {
+                    console.log(`Status: ${res.status}`);
+                    console.log('Body: ', res.data);
+                }).catch((err) => {
+                    console.error(err);
+                });
+
             break;
-        case 2: //(Wohnzimmer decke aus
+        case 2: // küche aus
+            console.log("turning kitchen lights on")
+            var body = {"on": false}
+
+            axios.put('http://192.168.178.136/api/-kft5OkDjuXXzojboZxroWDrwkyNqP57cSm6PJhl/groups/1/action', body)
+                .then((res) => {
+                    console.log(`Status: ${res.status}`);
+                    console.log('Body: ', res.data);
+                }).catch((err) => {
+                    console.error(err);
+                });
 
             break;
 
         default:
+            console.log("default case")
             break;
     }
     res.sendStatus(200)
